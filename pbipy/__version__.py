9f0aea453d07af60cc1c21fb3888c700beb484c5
Andrew Villazon <andrew.villazon@gmail.com>
2022-12-05 16:24:38 +1300

    Update version

--- pbipy/__version__.py
+++ pbipy/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "pbipy"
 __description__ = "A Power BI Rest API wrapper for Python."
 __url__ = "https://github.com/andrewvillazon/pbipy"
-__version__ = "0.2.3"
+__version__ = "1.0.0"
 __author__ = "Andrew Villazon"
 __author_email__ = "andrew.villazon@gmail.com"
 __license__ = "Apache 2.0"
