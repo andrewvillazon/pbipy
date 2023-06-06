82378ed97004f2b406bb09777978be87e10c40ad
Andrew Villazon <andrew.villazon@gmail.com>
2023-06-07 07:58:23 +1200

    Add put_dataset_user method and tests

--- tests/operationgroups/test_datasets.py
+++ tests/operationgroups/test_datasets.py
@@ -783,3 +783,49 @@ def test_post_dataset_user_in_group_raises_http_error(powerbi):
 
     with pytest.raises(HTTPError):
         powerbi.datasets.post_dataset_user_in_group("f089354e-8366-4e18-aea3-4cb4a3a50b48","cfafbeb1-8037-4d0c-896e-a46fb27ff229", dataset_user_access)
+
+
+@responses.activate
+def test_put_dataset_user(powerbi):
+    json_params = {
+        "identifier": "154aef10-47b8-48c4-ab97-f0bf9d5f8fcf",
+        "principalType": "Group",
+        "datasetUserAccessRight": "ReadReshare"
+        }
+    
+    responses.put(
+        "https://api.powerbi.com/v1.0/myorg/datasets/cfafbeb1-8037-4d0c-896e-a46fb27ff229/users"
+        ,match=[matchers.json_params_matcher(json_params)]
+    )
+
+    dataset_user_access = {
+        "identifier": "154aef10-47b8-48c4-ab97-f0bf9d5f8fcf",
+        "principalType": "Group",
+        "datasetUserAccessRight": "ReadReshare"
+        }
+    
+    powerbi.datasets.put_dataset_user("cfafbeb1-8037-4d0c-896e-a46fb27ff229", dataset_user_access)
+
+
+def test_put_dataset_user_raises_type_error(powerbi):
+    dataset_user_access = ["154aef10-47b8-48c4-ab97-f0bf9d5f8fcf","Group","ReadReshare"]
+
+    with pytest.raises(TypeError):
+        powerbi.datasets.put_dataset_user("cfafbeb1-8037-4d0c-896e-a46fb27ff229", dataset_user_access)
+
+
+@responses.activate
+def test_put_dataset_user_raises_http_error(powerbi):
+    responses.put(
+        "https://api.powerbi.com/v1.0/myorg/datasets/cfafbeb1-8037-4d0c-896e-a46fb27ff229/users",
+        status=501,
+    )
+    
+    dataset_user_access = {
+        "identifier": "154aef10-47b8-48c4-ab97-f0bf9d5f8fcf",
+        "principalType": "Group",
+        "datasetUserAccessRight": "ReadReshare"
+        }
+    
+    with pytest.raises(HTTPError):
+        powerbi.datasets.put_dataset_user("cfafbeb1-8037-4d0c-896e-a46fb27ff229", dataset_user_access)
