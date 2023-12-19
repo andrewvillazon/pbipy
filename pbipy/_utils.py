e4f0db62278e366a6e568d7701c3615cf7b40f4b
Andrew Villazon <andrew.villazon@gmail.com>
2023-12-19 22:28:19 +1300

    Move remaining functions from utils to _utils

--- pbipy/_utils.py
+++ pbipy/_utils.py
@@ -1,7 +1,161 @@
 """Utility functions that are consumed internally by pbipy."""
 
+from pathlib import Path
+import re
+from typing import TYPE_CHECKING
 
 from requests import RequestException, Response, Session
+
+if TYPE_CHECKING:
+    from pbipy.resources import Resource
+
+
+def build_path(
+    path_format: str,
+    *identifiers: "str | Resource",
+) -> str:
+    """
+    Build a path to a resource from the supplied components.
+
+    Parameters
+    ----------
+    `path_format` : `str`
+        The resource format with bracketed placeholders, e.g., `reports/{}/datasources`
+    `*identifiers` : `str | Resource`
+        Ids or Resource Objects with an `id` attribute. The provided Ids
+        will be added to the path according `path_format`.
+
+    Returns
+    -------
+    `str`
+        Path to a resource.
+
+    """
+
+    ids = []
+
+    for identifier in identifiers:
+        try:
+            ids.append(identifier.id)
+        except AttributeError:
+            ids.append(identifier)
+
+    path = path_format.format(*ids)
+
+    return path
+
+
+def to_snake_case(s):
+    pattern = re.compile("((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))")
+    return pattern.sub(r"_\1", s).lower()
+
+
+def to_camel_case(text):
+    s = text.replace("-", " ").replace("_", " ")
+    s = s.split()
+
+    if len(text) == 0:
+        return text
+
+    return s[0] + "".join(i.capitalize() for i in s[1:])
+
+
+def remove_no_values(
+    d: dict,
+) -> dict:
+    """
+    Recursively remove keys with a value of `None` or `{}` from a dictionary.
+    If the result of removing `None` is `{}`, then this is removed until
+    only keys with values remain.
+
+    Parameters
+    ----------
+    `d` : `dict`
+        dict to remove None or empty dicts from.
+
+    Returns
+    -------
+    `dict`
+        dict with `None` or `{}` removed.
+    """
+
+    new_d = {}
+
+    for k, v in d.items():
+        if isinstance(v, dict):
+            v = remove_no_values(v)
+        if not v in (None, {}):
+            new_d[k] = v
+
+    return new_d
+
+
+def file_path_from_components(
+    file_name: str,
+    extension: str,
+    directory: str | Path = None,
+):
+    """
+    Create a file path from file path components.
+
+    Parameters
+    ----------
+    `file_name` : `str`
+        Name of the file (without extension).
+    `extension` : `str`
+        The file extension.
+    `directory` : `str | Path`, optional
+        Directory of the file.
+
+    Returns
+    -------
+    `Path`
+        File path to the file.
+
+    """
+
+    if isinstance(directory, str):
+        f_dir = Path(directory)
+    elif directory is None:
+        f_dir = Path()  # cwd
+    else:
+        f_dir = directory
+
+    # Be a bit more flexible and accept .ext
+    if extension[0] == ".":
+        f_ext = extension[1:]
+    else:
+        f_ext = extension
+
+    return f_dir / f"{file_name}.{f_ext}"
+
+
+def to_identifier(
+    s: str,
+) -> str:
+    """
+    Convert a string into a valid Python identifier, e.g., variable or
+    attribute name.
+
+    Parameters
+    ----------
+    `s` : `str`
+        String to convert.
+
+    Returns
+    -------
+    `str`
+        Converted string.
+
+    """
+
+    # Remove leading characters until letter or underscore
+    s = re.sub("^[^a-zA-Z_]+", "", s)
+
+    # Replace invalid characters
+    s = re.sub("[^0-9a-zA-Z_]", "_", s)
+
+    return s
 
 
 def parse_raw(
